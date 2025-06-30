898e32b33ff61ed914c25c3db3525f7edb3564d3
Cong Le <longchike@gmail.com>
2025-06-30 01:06:55 -0700

    The setup

---
+++ Quantity_in_IIT/Quantity_in_IITApp.swift
@@ -0,0 +1,17 @@
+//
+//  Quantity_in_IITApp.swift
+//  Quantity_in_IIT
+//
+//  Created by Cong Le on 6/30/25.
+//
+
+import SwiftUI
+
+@main
+struct Quantity_in_IITApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
